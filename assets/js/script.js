ed6b4eaa6eeeaef0a4b1afdbb286ecb3212e93f6
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-04-08 11:37:41 -0400

    api key working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-const requestUrl = "https://openweathermap.org/api";
+const requestUrl =
+  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=cc1961620c07d9c2d3b1a593bf9ec1b1";
 
 // checking
 fetch(requestUrl)
   .then(function (response) {
+    if (response.ok) {
+      console.log("response found");
+    }
     return response.json;
   })
   .then(function (data) {
-    console.log(data);
+    for (let d of data) {
+      console.log(d);
+    }
   });
+
+let today = dayjs().format("DD/MM/YYYY");
+
+$("#today-date").text(today);
