0533d8e19b8fa0cb784f66cbf01e0738148f6b12
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-04-09 10:20:46 -0400

    Main weather card updated with live data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,41 @@
-const requestUrl =
-  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=cc1961620c07d9c2d3b1a593bf9ec1b1";
+const searchBtn = document.querySelector("#search-city");
+const cityInput = document.querySelector("#search");
+const cityHeadCard = document.querySelector("#required-city h2");
+const cityTemp = document.querySelector("#city-temp");
+const cityWind = document.querySelector("#city-wind");
+const cityHumidity = document.querySelector("#city-humidity");
 
-// checking
-fetch(requestUrl)
-  .then(function (response) {
+const formSubmitHandler = (event) => {
+  event.preventDefault();
+
+  const cityName = cityInput.value.trim().toLowerCase();
+
+  if (cityName) {
+    selectedCityWeather(cityName);
+    cityInput.value = "";
+  } else {
+    alert("Enter the city name");
+  }
+};
+
+const selectedCityWeather = (cityName) => {
+  cityHeadCard.textContent = cityName.toUpperCase();
+
+  const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=cc1961620c07d9c2d3b1a593bf9ec1b1`;
+
+  fetch(baseUrl).then(function (response) {
     if (response.ok) {
-      console.log("response found");
-    }
-    return response.json;
-  })
-  .then(function (data) {
-    for (let d of data) {
-      console.log(d);
+      response.json().then(function (data) {
+        console.log(data);
+        console.log(data.main);
+        cityTemp.textContent = ` ${data.main.temp}F`;
+        cityWind.textContent = ` ${data.wind.speed}MPH`;
+        cityHumidity.textContent = ` ${data.main.humidity}%`;
+      });
+    } else {
+      alert("error in city name");
     }
   });
+};
 
-let today = dayjs().format("DD/MM/YYYY");
-
-$("#today-date").text(today);
+searchBtn.addEventListener("click", formSubmitHandler);
