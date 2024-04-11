d4a9671abf16a6a6c6c46e815ffe081697495036
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-04-11 01:06:13 -0400

    selection of the button changed for city buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const cityHeadCard = document.querySelector("#required-city h2");
 const cityTemp = document.querySelector("#city-temp");
 const cityWind = document.querySelector("#city-wind");
 const cityHumidity = document.querySelector("#city-humidity");
-const cityBtn = document.querySelectorAll(".btn");
+const cityBtn = document.querySelectorAll(".city-btn");
 
 const apiKey = `cc1961620c07d9c2d3b1a593bf9ec1b1`;
 
@@ -24,14 +24,14 @@ const formSubmitHandler = (event) => {
 const inputCityWeather = (cityName) => {
     cityHeadCard.textContent = cityName.toUpperCase();
 
-    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric`;
+    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial`;
     const userUrl = baseUrl + `&appid=${apiKey}`;
 
     fetch(userUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
-                cityTemp.textContent = ` ${data.main.temp} C`;
+                cityTemp.textContent = ` ${data.main.temp} F`;
                 cityWind.textContent = ` ${data.wind.speed} MPH`;
                 cityHumidity.textContent = ` ${data.main.humidity} %`;
                 console.log(data.weather[0].icon);
